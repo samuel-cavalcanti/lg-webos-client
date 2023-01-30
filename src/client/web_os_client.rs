79c214bb7431e48f47a5c7599d8da4d8b6271aa1
samuel-cavalcanti <scavalcanti111@gmail.com>
2023-01-30 11:10:08 -0300

    impl new traits on WebOsClient

--- src/client/web_os_client.rs
+++ src/client/web_os_client.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use log::debug;
 
 use serde_json::{json, Value};
@@ -11,34 +12,47 @@ use crate::lg_command::pointer_input_commands::PointerInputCommand;
 use crate::lg_command::LGCommandRequest;
 
 use super::web_os_network::{WebOsSocketTvSend, WebOsTvRequestCommunication, WebSocketErrorAction};
+use super::{SendLgCommandRequest, SendPointerCommandRequest};
 
 /// Client for interacting with TV
 pub struct WebOsClient {
     pub key: Option<String>,
     request_sender: Box<dyn WebOsTvRequestCommunication>,
-    pointer_input_sender: Box<dyn WebOsSocketTvSend>,
+    pointer_input_sender: Option<Box<dyn WebOsSocketTvSend>>,
 }
 
 impl WebOsClient {
     pub async fn connect(config: WebOsClientConfig) -> Result<WebOsClient, WebSocketErrorAction> {
-        let mut tv_connection = WebOsMultiThreadSocketConnection::connect_to_tv(config).await?;
-        // let pointer_input_connection = WebOsPointerInputConnection::connect(&tv_connection.sender).await?;
+        let tv_connection = WebOsMultiThreadSocketConnection::connect_to_tv(config).await?;
         debug!("connected with TV");
-        let pointer_input_sender =
-            InputPointerSocketConnection::try_to_connect(&mut tv_connection.request_sender).await?;
 
         let client = WebOsClient {
             key: Some(tv_connection.key),
             request_sender: tv_connection.request_sender,
 
-            pointer_input_sender,
+            pointer_input_sender: None,
         };
 
         Ok(client)
     }
 
-    /// Sends single lg_command and waits for response
-    pub async fn send_command_to_tv<R: LGCommandRequest>(
+    async fn try_to_connect_pointer_client(&mut self) -> Result<(), WebSocketErrorAction> {
+        if self.pointer_input_sender.is_some() {
+            return Ok(());
+        }
+
+        let pointer_input_sender =
+            InputPointerSocketConnection::try_to_connect(&mut self.request_sender).await?;
+
+        self.pointer_input_sender = Some(pointer_input_sender);
+
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl SendLgCommandRequest for WebOsClient {
+    async fn send_lg_command_to_tv<R: LGCommandRequest>(
         &mut self,
         cmd: R,
     ) -> Result<Value, WebSocketErrorAction> {
@@ -51,13 +65,37 @@ impl WebOsClient {
 
         Ok(promise.await)
     }
+}
 
-    pub async fn send_pointer_input_command_to_tv<R: PointerInputCommand>(
+#[async_trait]
+impl SendLgCommandRequest for &mut WebOsClient {
+    async fn send_lg_command_to_tv<R: LGCommandRequest>(
+        &mut self,
+        cmd: R,
+    ) -> Result<Value, WebSocketErrorAction> {
+        // let mut sender = &mut self.tv_sender;
+        let request = cmd.to_command_request();
+        let promise = self
+            .request_sender
+            .send_json_request(json!(request))
+            .await?;
+
+        Ok(promise.await)
+    }
+}
+
+#[async_trait]
+impl SendPointerCommandRequest for WebOsClient {
+    async fn send_pointer_input_command_to_tv<R: PointerInputCommand>(
         &mut self,
         cmd: R,
     ) -> Result<(), WebSocketErrorAction> {
-        self.pointer_input_sender
-            .send_text(cmd.to_request_string())
-            .await
+        self.try_to_connect_pointer_client().await?;
+
+        let text = cmd.to_request_string();
+        match self.pointer_input_sender {
+            Some(ref mut pointer_client) => pointer_client.send_text(text).await,
+            None => Err(WebSocketErrorAction::Fatal),
+        }
     }
 }
